3575f485cd6dd278c883d80497d20a8e8c5e91f9
kcvivek <kcvivek99@gmail.com>
2016-10-14 23:13:29 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "strconv"
+
+    "github.com/gorilla/mux"
+    
+    //"github.com/braintree/manners"
+    //"github.com/kcvivek/bits-mtech/app/handlers"
+    //"github.com/kcvivek/bits-mtech/app/health"
+    //"github.com/kcvivek/bits-mtech/app/user"
+)
+
+func main() {
+
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    router.HandleFunc("/fact/{factId}", FactorialCall)
+    
+    log.Fatal(http.ListenAndServe(":8085", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome to the Factorial Portal!")
+}
+
+
+func FactorialCall(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    num  := vars["factId"]
+    i, err := strconv.Atoi(num)
+
+    if err != nil {
+       log.Fatal(err)
+    }
+
+    var f uint64
+    if (i > 20) {
+      f = 0
+      fmt.Frintf(w, "Input error, can not calculate factorial for number greather than 20")
+    } else {
+        f = Factorial(uint64(i))
+    }
+    
+    fmt.Fprintf(w, "Factorial for number %d is: %d \nBye, see you in next run..\n", i, f)
+}
+
+func Factorial(n uint64)(result uint64) {
+    if (n > 0) {
+       result = n * Factorial(n-1)
+       return result
+     }
+     return 1
+}
